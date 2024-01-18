ea484e7f7fbc89064bc7e0699708676f5a9b4f00
rayleigh420 <nhatduy0409@gmail.com>
2024-01-18 11:14:54 +0700

    construct background for header ui design

--- libs/presentational/src/organisms/Header.tsx
+++ libs/presentational/src/organisms/Header.tsx
@@ -1,5 +1,89 @@
+// 'use client';
+
+import logo from '../assets/logo.png';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 md:px-[2.42rem] md:pt-[1.2rem] lg:px-6 py-2.5 lg:bg-red-500">
+        <div className="flex flex-wrap justify-between items-center max-w-full min-w-full">
+          <a href="#" className="flex items-center ">
+            <img
+              src={logo.src}
+              className="md:h-[3.6rem] lg:h-10 md:mr-[1.29rem] lg:mr-[0.9rem]"
+              alt="DeToi Logo"
+            />
+            <span className="self-center md:text-[1.42rem] lg:text-[1rem] font-semibold whitespace-nowrap underline italic">
+              detoi@sunrise.continent.com
+            </span>
+          </a>
+
+          <div className="flex items-center ">
+            <div>Hello</div>
+            {/* <div
+              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+              id="mobile-menu-2"
+            >
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Company
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Marketplace
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div> */}
+            <button className="text-black md:text-[1.14rem] lg:text-[0.8rem] font-bold md:px-[2.28rem] md:py-[0.85rem] lg:px-[1.6rem] lg:py-[0.6rem] border-solid border-3 border-black rounded-md ">
+              Đăng nhập
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
