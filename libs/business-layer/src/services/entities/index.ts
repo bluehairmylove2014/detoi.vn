46c9646825aa47db9c95ad402ba3547e1bddb3de
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2024-01-19 00:03:54 +0700

    FIX/Remove unnecessary entities

--- libs/business-layer/src/services/entities/index.ts
+++ libs/business-layer/src/services/entities/index.ts
@@ -1,7 +1,3 @@
 export * from './account';
-export * from './ads';
-export * from './report';
-export * from './location';
 export * from './pagination';
 export * from './breadcrumb';
-export * from './setting';
