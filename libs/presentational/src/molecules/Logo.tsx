b98c051df874f9628edd988fdf2258a803940e96
rayleigh420 <nhatduy0409@gmail.com>
2024-01-23 10:09:43 +0700

    Update header

--- libs/presentational/src/molecules/Logo.tsx
+++ libs/presentational/src/molecules/Logo.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import React from 'react';
-import logo from '../assets/detoi.logo.svg';
+import logo from '@assets/detoi.logo.svg';
 
 export default function Logo() {
   return (
@@ -10,9 +12,9 @@ export default function Logo() {
         alt="DeToi Logo"
       />
       <span className="self-center hidden lg:inline text-[20px] lg:text-[1rem] font-semibold whitespace-nowrap underline italic">
-        detoi@sunrise.continent.com
+        detoi@sunrise.continent
       </span>
-      <span className="self-center inline lg:hidden text-[20px] font-semibold whitespace-nowrap underline italic">
+      <span className="self-center inline lg:hidden text-[18px] font-semibold whitespace-nowrap underline italic">
         detoi
       </span>
     </a>
--- libs/presentational/src/organisms/Header.tsx
+++ libs/presentational/src/organisms/Header.tsx
@@ -1,53 +1,57 @@
-// 'use client';
-
 import Logo from '@presentational/molecules/Logo';
-import Help from '../assets/icon/help.svg';
 import CWEBFAIcon from '@presentational/atoms/CWEBFAIcon';
 import CustomButton from '@presentational/atoms/CustomButton';
 import IconButton from '@presentational/atoms/IconButton';
+import Link from 'next/link';
 
 function Header() {
   return (
-    <div className="xl:container">
-      <header>
+    <header>
+      <div className="xl:container">
         <nav className="bg-white px-[30px] py-[18px]">
           <div className="flex flex-wrap justify-between items-center max-w-full min-w-full">
             <Logo />
             <div className=" flex items-center justify-center md:gap-0">
               <div className="hidden md:block">
-                <CustomButton style="btn-white-black">
-                  <CWEBFAIcon
-                    iconName="faCircleQuestion"
-                    className="text-[16px]"
-                  />
-                  <span className="ml-1.5 text-[16px]">Hỗ trợ</span>
-                </CustomButton>
+                <Link href={'#'}>
+                  <CustomButton style="btn-white-black">
+                    <CWEBFAIcon
+                      iconName="faCircleQuestion"
+                      className="text-[16px]"
+                    />
+                    <span className="ml-1 text-[16px]">Hỗ trợ</span>
+                  </CustomButton>
+                </Link>
               </div>
               <div className="hidden md:block ">
-                <CustomButton style="btn-white-black">
-                  <span className="text-[16px]">Trở thành Freelancer</span>
-                </CustomButton>
+                <Link href={'#'}>
+                  <CustomButton style="btn-white-black">
+                    <span className="text-[16px]">Trở thành Freelancer</span>
+                  </CustomButton>
+                </Link>
               </div>
               <div className="hidden md:block ">
-                <CustomButton style="btn-white-black">
-                  <span className="text-[16px]">Chính sách</span>
-                </CustomButton>
+                <Link href={'#'}>
+                  <CustomButton style="btn-white-black">
+                    <span className="text-[16px]">Chính sách</span>
+                  </CustomButton>
+                </Link>
               </div>
               <div className="w-29">
-                <CustomButton style="btn-white-black-border">
-                  <span className="text-[16px] mx-1">Đăng nhập</span>
-                </CustomButton>
+                <Link href={'#'}>
+                  <CustomButton style="btn-white-black-border">
+                    <span className="text-[16px]">Đăng nhập</span>
+                  </CustomButton>
+                </Link>
               </div>
               <div className="md:hidden">
-                <CustomButton style="btn-white-black">
-                  <CWEBFAIcon iconName="faBar" className="text-2xl" />
-                </CustomButton>
+                <IconButton iconName="faBar" />
               </div>
             </div>
           </div>
         </nav>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
 
