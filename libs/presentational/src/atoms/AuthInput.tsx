2432c298bbe3d425229b59ee461119077f28d5e6
rayleigh420 <nhatduy0409@gmail.com>
2024-01-28 11:26:36 +0700

    Add some filed for AuthInput

--- libs/presentational/src/atoms/AuthInput.tsx
+++ libs/presentational/src/atoms/AuthInput.tsx
@@ -15,10 +15,10 @@ const inputDefinition = {
 };
 
 export type authInputParams = {
-  name?: string;
-  label?: string;
-  type?: 'EMAIL' | 'PASSWORD';
-  control?: any;
+  name: string;
+  label: string;
+  type: 'EMAIL' | 'PASSWORD';
+  control: any;
   disabled?: boolean;
   onChange?: (value: string) => void;
 };
@@ -33,10 +33,39 @@ const AuthInput = ({
 }: authInputParams) => {
   return (
     <div className="relative bg-transparent rounded shadow-lg h-10 w-full mt-[30px] ">
-      <label className="bg-white bg-opacity-80 text-[0.65rem] absolute left-3 z-50 -translate-y-1/2 font-semibold flex flex-row items-center justify-start px-1">
-        <span className="">{label}</span>
-      </label>
-      <input className="text-xs border border-midgray outline-none bg-transparent px-3 w-full h-full rounded-sm" />
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <>
+            <label className="bg-white bg-opacity-80 text-[0.65rem] absolute left-3 z-50 -translate-y-1/2 font-semibold flex flex-row items-center justify-start px-1">
+              htmlFor={name}
+              <span className="">{label}</span>
+            </label>
+            <input
+              className="text-xs border border-midgray outline-none bg-transparent px-3 w-full h-full rounded-sm"
+              type={inputDefinition[type].type}
+              id={name}
+              disabled={disabled}
+              autoComplete="off"
+              {...field}
+              onChange={(e) => {
+                const value = e.target.value;
+                field.onChange(value);
+                onChange && onChange(value);
+              }}
+              onFocus={() => {
+                // addClass(labelSpanRef.current, 'opacity-0');
+              }}
+              onBlur={(e) => {
+                if (e.target.value.trim().length === 0) {
+                  //   removeClass(labelSpanRef.current, 'opacity-0');
+                }
+              }}
+            />
+          </>
+        )}
+      />
     </div>
   );
 };
