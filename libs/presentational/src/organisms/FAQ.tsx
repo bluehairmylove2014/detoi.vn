d89aea54063124dbcfa85bafa2f12f9b212022bc
TanKaKa123 <hongtan1422002@gmail.com>
2024-01-29 21:22:56 +0700

    remove open multiple faq

--- libs/presentational/src/organisms/FAQ.tsx
+++ libs/presentational/src/organisms/FAQ.tsx
@@ -1,5 +1,6 @@
-'use client';
-import React, { useCallback, useState } from 'react';
+'use client'; // Added 'use strict' to enforce better coding practices
+
+import React, { useCallback, useMemo, useState } from 'react';
 import { Text } from '@presentational/atoms';
 import IconButton from '@presentational/atoms/IconButton';
 
@@ -7,6 +8,8 @@ type TAccordionProps = {
   idx: number;
   title: string;
   content: string;
+  onClick: () => void;
+  isOpen: Boolean;
 };
 
 type TFaqListContentProps = {
@@ -17,20 +20,14 @@ type TFaqListContentProps = {
 };
 
 const Accordion: React.FC<TAccordionProps> = React.memo(
-  ({ idx, title, content }) => {
-    const [isOpen, setIsOpen] = useState<Boolean>(false);
-
-    const handleClick = useCallback(() => {
-      setIsOpen(!isOpen);
-    }, []);
-
+  ({ idx, title, content, onClick, isOpen = false }) => {
     return (
       <>
         <div
-          onClick={handleClick}
+          onClick={onClick}
           className="flex flex-row justify-between items-center font-semibold p-3 cursor-pointer"
         >
-          <Text align='left' style="font-bold">
+          <Text align="left" style="font-bold">
             {title}
           </Text>
           <IconButton
@@ -40,11 +37,11 @@ const Accordion: React.FC<TAccordionProps> = React.memo(
           />
         </div>
         <div
-          className={`border-l-2 border-purple-600 overflow-hidden max-h  ${
+          className={`border-l-2 border-purple-600 overflow-hidden max-h ${
             isOpen ? 'border-t border-gray-400 max-h-full' : 'max-h-0'
           }`}
         >
-          <Text align='left' size="sm" style="p-3 text-gray">
+          <Text align="left" size="sm" style="p-3 text-gray">
             {content}
           </Text>
         </div>
@@ -55,9 +52,21 @@ const Accordion: React.FC<TAccordionProps> = React.memo(
 
 const FAQ: React.FC<TFaqListContentProps> = React.memo(
   ({ faqListContent = [] }) => {
+    const [openItemWithIndex, setOpenItemWithIndex] = useState<number>(-1); // Corrected the type to 'number'
+    const handleClick = useCallback(
+      (index: number) => {
+        if (index === openItemWithIndex) {
+          setOpenItemWithIndex(-1);
+        } else {
+          setOpenItemWithIndex(index);
+        }
+      },
+      [openItemWithIndex]
+    );
+
     return (
       <main className="p-5">
-        <div className="flex justify-center items-start my-2 ">
+        <div className="flex justify-center items-start my-2">
           <div className="w-full sm:w-10/12 md:w-1/2 my-1">
             <ul
               className="flex flex-col border-collapse border-solid border-gray rounded-md"
@@ -72,9 +81,11 @@ const FAQ: React.FC<TFaqListContentProps> = React.memo(
                   key={index}
                 >
                   <Accordion
+                    isOpen={index === openItemWithIndex}
                     idx={index}
                     title={faqItem.title}
                     content={faqItem.content}
+                    onClick={() => handleClick(index)}
                   />
                 </li>
               ))}
