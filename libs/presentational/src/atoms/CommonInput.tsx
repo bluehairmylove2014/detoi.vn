f6a2e03bf240e47bf23f404d11cd711bf90c8aad
TanKaKa123 <hongtan1422002@gmail.com>
2024-02-15 22:06:49 +0700

    reformat style common input

--- libs/presentational/src/atoms/CommonInput.tsx
+++ libs/presentational/src/atoms/CommonInput.tsx
@@ -7,14 +7,6 @@ const inputTypePropsMapping = {
   shortText: 'text',
   longText: 'text',
 };
-const styleThemeMapping = {
-  clean: {
-    div: 'relative border-b-2 border-solid bg-transparent w-full h-12 focus:!border-b-stone',
-    label:
-      'absolute top-1/2 transform -translate-y-1/2 left-0 font-semibold text-gray focus:!text-stone z-10',
-    input: 'outline-none border-none w-full h-full relative bg-transparent z-0',
-  },
-};
 
 type commonInputProps = {
   theme: 'clean';
@@ -49,14 +41,14 @@ function CommonInput({
   };
   return (
     <div
-      className={styleThemeMapping[theme].div}
+      className="relative border-b-2 border-solid bg-transparent w-full h-12 focus:!border-b-stone"
       ref={divRef}
       onFocus={handleInputFocus}
       onBlur={handleInputBlur}
     >
       <label
         htmlFor={id}
-        className={`${styleThemeMapping[theme].label} ${
+        className={`absolute top-1/2 transform -translate-y-1/2 left-0 font-semibold text-gray focus:!text-stone z-10 ${
           watcher?.trim().length > 0
             ? 'opacity-0 invisible'
             : 'opacity-100 visible'
@@ -64,9 +56,9 @@ function CommonInput({
       >
         {label}
       </label>
-      <input 
+      <input
         id={id}
-        className={styleThemeMapping[theme].input}
+        className='outline-none border-none w-full h-full relative bg-transparent z-0'
         type={inputTypePropsMapping[type]}
         {...register(inputName)}
       />
