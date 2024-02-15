74a57929be604058644320d3ed360b308c1a1c82
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2024-02-15 12:14:36 +0700

    FIX/Responsive Customer Web Header

--- libs/presentational/src/molecules/Logo.tsx
+++ libs/presentational/src/molecules/Logo.tsx
@@ -7,15 +7,15 @@ export default function Logo() {
     <Link href="#" className="flex items-center">
       <Image
         src={logo}
-        className="mr-[1.2rem] md:block md:h-[50px] lg:h-[50px] md:mr-[18px]"
-        alt="DeToi Logo"
+        className="mr-[1.2rem] md:mr-[18px]"
+        alt="DeToi"
         priority={true}
+        quality={100}
+        width={40}
+        height={40}
       />
-      <span className="self-center hidden lg:inline text-[20px] lg:text-[1rem] font-semibold whitespace-nowrap underline italic">
+      <span className="self-center hidden md:block lg:text-base font-semibold whitespace-nowrap underline italic">
         detoi@sunrise.continent
-      </span>
-      <span className="self-center inline lg:hidden text-[18px] font-semibold whitespace-nowrap underline italic">
-        detoi
       </span>
     </Link>
   );
---
+++ libs/presentational/src/molecules/ShortenNavSideBar.tsx
@@ -0,0 +1,53 @@
+'use client';
+import CWEBFAIcon from '@presentational/atoms/CWEBFAIcon';
+import CustomButton from '@presentational/atoms/CustomButton';
+import IconButton from '@presentational/atoms/IconButton';
+import { toggleClass } from '@utils/helpers';
+import { useRef } from 'react';
+
+function ShortenNavSideBar() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <IconButton
+        iconName="faBar"
+        size="xl"
+        onClick={() => toggleClass(ref.current, 'active')}
+      />
+      <div
+        ref={ref}
+        className="fixed z-40 top-0 right-0 h-full w-[min(100vw,500px)] hiddenByOpacity bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] flex flex-col items-center justify-start pt-8 transition-all"
+      >
+        <div className="w-full h-fit flex flex-row justify-around">
+          <h3 className="whitespace-nowrap flex-grow ml-5 select-none">
+            Danh mục
+          </h3>
+          <div className="w-fit flex-shrink">
+            <CustomButton
+              style="btn-white-black"
+              type="button"
+              onClick={() => toggleClass(ref.current, 'active')}
+            >
+              <CWEBFAIcon iconName="faXmarkCircle" className="text-base" />
+            </CustomButton>
+          </div>
+        </div>
+        <hr className="h-[1px] w-[calc(100%-30px)] bg-zinc text-transparent px-5 mb-8" />
+        <div className="mb-4">
+          <CustomButton style="btn-white-black" type="link" href="#">
+            <span className="text-base">Chính sách</span>
+          </CustomButton>
+        </div>
+        <div className="mb-4">
+          <CustomButton style="btn-white-black" type="link" href="#">
+            <CWEBFAIcon iconName="faCircleQuestion" className="text-base" />
+            <span className="ml-2 text-base">Hỗ trợ</span>
+          </CustomButton>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShortenNavSideBar;
