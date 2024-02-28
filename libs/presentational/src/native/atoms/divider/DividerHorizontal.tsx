805652d2f30a4cc60bbd5e77121dd83bfb6f15b8
TanKaKa123 <hongtan1422002@gmail.com>
2024-02-28 21:52:28 +0700

    fix comment

---
+++ libs/presentational/src/native/atoms/divider/DividerHorizontal.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+
+type DividerProps = {
+  width?: number;
+  orientation?: 'horizontal' | 'vertical';
+  color?: string;
+  dividerStyle?: any;
+};
+
+export const DividerHorizontal: React.FC<DividerProps> = ({
+  width = 1,
+  orientation = 'horizontal',
+  color = '#DFE4EA',
+  dividerStyle,
+}) => {
+  const dividerStyles = [
+    { width: orientation === 'horizontal' ? '100%' : width },
+    { height: orientation === 'vertical' ? '100%' : width },
+    { backgroundColor: color },
+    dividerStyle,
+  ];
+
+  return <View style={dividerStyles} />;
+};
