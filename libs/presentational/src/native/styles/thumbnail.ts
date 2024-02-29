0aac890db7fdd800b8fe314c8838a678ccc85753
rayleigh420 <nhatduy0409@gmail.com>
2024-02-29 21:44:42 +0700

    UPDATE/ Atom thumbnail more theme

--- libs/presentational/src/native/styles/thumbnail.ts
+++ libs/presentational/src/native/styles/thumbnail.ts
@@ -5,5 +5,15 @@ export const ThumbnailSectionStyle = StyleSheet.create({
         height: 160, 
         width: 160, 
         borderRadius: 5
+    },
+    medium: {
+        height: 106, 
+        width: 106, 
+        borderRadius: 4
+    },
+    small: {
+        height: 80, 
+        width: 80, 
+        borderRadius: 3
     }
 })
