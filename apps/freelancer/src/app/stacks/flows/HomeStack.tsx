2a35abcc760a4525063139892b7e4ed67e16b9d3
TanKaka123 <hongtan1422002@gmail.com>
2024-03-23 12:59:16 +0700

    add screen to home stack

--- apps/freelancer/src/app/stacks/flows/HomeStack.tsx
+++ apps/freelancer/src/app/stacks/flows/HomeStack.tsx
@@ -3,16 +3,22 @@ import { customerRootStackParamList } from '../../../config';
 
 // Import screen component here
 import HomeScreen from '../../../screens/homeFlow/home/Home';
+import IntroScreen from '../../../screens/homeFlow/intro/Intro';
 
 const Stack = createNativeStackNavigator<customerRootStackParamList>();
 export const HomeStack = ({ isLogged }: { isLogged: boolean }) => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Intro">
       {/* Then, put stack.screen for your screen component here (in the end) */}
 
       <Stack.Screen
         component={HomeScreen}
         name="Home"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        component={IntroScreen}
+        name="Intro"
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
