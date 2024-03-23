40d002a8565b7c04c24117d0e9a3a9a7133d8fb0
TanKaka123 <hongtan1422002@gmail.com>
2024-03-23 13:31:24 +0700

    add splash intro screen freelancer

--- apps/freelancer/src/screens/homeFlow/intro/Intro.tsx
+++ apps/freelancer/src/screens/homeFlow/intro/Intro.tsx
@@ -23,7 +23,8 @@ const DotPager = React.memo(({ isSelected }: { isSelected: boolean }) => {
         borderRadius: 99,
         backgroundColor: isSelected
           ? COLOR_PALETTE.primary
-          : COLOR_PALETTE.pastel,
+          : COLOR_PALETTE.black,
+        opacity: isSelected ? 1 : 0.3,
       }}
     />
   );
@@ -34,22 +35,19 @@ const CONTENTS_PAGINATION = [
     title: 'Đa dạng dịch vụ',
     description:
       'Vô vàn dịch vụ đang cần bạn, hãy chọn các dịch vụ bạn có thể làm',
-    image:
-      'https://res.cloudinary.com/drnqf7lrb/image/upload/v1711167887/ezaphy5i8e6jnrhej30u.png',
+    image: require('../../../../assets/splash-intro-1.png'),
   },
   {
     title: 'Bạn tự định giá',
     description:
       'Phí dịch vụ do bạn hoàn toàn quyết định. Dịch vụ bạn làm, giá trị bạn định',
-    image:
-      'https://res.cloudinary.com/drnqf7lrb/image/upload/v1711167883/w8ocs8o575h7hsruqvgh.png',
+    image: require('../../../../assets/splash-intro-2.png'),
   },
   {
     title: 'Dễ dàng kết nối',
     description:
       'Hỗ trợ tìm và nhận đơn dễ dàng hơn phương pháp truyền thống gấp 2 lần',
-    image:
-      'https://res.cloudinary.com/drnqf7lrb/image/upload/v1711167883/aqi15k8anmctbcobtx5b.png',
+    image: require('../../../../assets/splash-intro-3.png'),
   },
 ];
 
@@ -73,9 +71,7 @@ const Intro = React.memo(() => {
         {
           <View style={bodyIntroScreenStyles.bodyWrapper}>
             <Image
-              source={{
-                uri: CONTENTS_PAGINATION[indexPagination].image,
-              }}
+              source={CONTENTS_PAGINATION[indexPagination].image}
               style={bodyIntroScreenStyles.image}
             />
             <VerticalSpacer size="xl" />
