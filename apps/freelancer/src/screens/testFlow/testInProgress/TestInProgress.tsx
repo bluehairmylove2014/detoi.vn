933dcd0deeb146d58dc15ac44661667b6cc2e8d5
ptminngoc <pngoc0105@gmail.com>
2024-03-25 15:36:09 +0700

    ADD/Animation For Test Flow

--- apps/freelancer/src/screens/testFlow/testInProgress/TestInProgress.tsx
+++ apps/freelancer/src/screens/testFlow/testInProgress/TestInProgress.tsx
@@ -1,7 +1,7 @@
 import { freelancerScreensList } from '@constants/freelancerScreens';
 import CustomerTemplate from '@present-native/templates/CustomerTemplate';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { View } from 'react-native';
+import { Animated, Easing, View } from 'react-native';
 import {
   AnswerButton,
   Paragraph,
@@ -10,7 +10,7 @@ import {
   Title,
   VerticalSpacer,
 } from '@present-native/atoms';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useAuthNavigation } from '@business-layer/business-logic/non-service-lib/navigation';
 import { TestInProgressStyle } from './styles';
 
@@ -33,6 +33,7 @@ const TestInProgress: React.FC<
     'normal',
     'normal',
   ]);
+  const opacity = useRef(new Animated.Value(1)).current;
 
   const handleAnswerSelection = (
     indexAnswerCurrent: number,
@@ -77,18 +78,32 @@ const TestInProgress: React.FC<
   };
 
   const handleNextQuestion = () => {
-    if (indexQuestionCurrent < questionList.length - 1) {
-      const newStateAnswerList = [...stateAnswerList];
+    Animated.timing(opacity, {
+      toValue: 0.5,
+      duration: 200,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(() => {
+      if (indexQuestionCurrent < questionList.length - 1) {
+        const newStateAnswerList = [...stateAnswerList];
 
-      // eslint-disable-next-line array-callback-return
-      questionList[indexQuestionCurrent].answers.map((stateAnswer, index) => {
-        newStateAnswerList[index] = 'normal';
-      });
+        // eslint-disable-next-line array-callback-return
+        questionList[indexQuestionCurrent].answers.map((stateAnswer, index) => {
+          newStateAnswerList[index] = 'normal';
+        });
 
-      setStateAnswerList(newStateAnswerList);
-      setIsDisableContinue(true);
-      setIndexQuestionCurrent(indexQuestionCurrent + 1);
-    } else checkPoint(currentPoint);
+        setStateAnswerList(newStateAnswerList);
+        setIsDisableContinue(true);
+        setIndexQuestionCurrent(indexQuestionCurrent + 1);
+      } else checkPoint(currentPoint);
+
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    });
   };
 
   useEffect(() => {
@@ -123,51 +138,59 @@ const TestInProgress: React.FC<
         />
 
         <VerticalSpacer size="xxxl" />
-        <View style={TestInProgressStyle.questionContainer}>
-          <>
-            <View style={TestInProgressStyle.questionPointContainer}>
-              <Paragraph theme="smallMedium" color="gray">
-                Câu hỏi {indexQuestionCurrent + 1}
+        <Animated.View
+          style={{
+            opacity: opacity,
+          }}
+        >
+          <View style={TestInProgressStyle.questionContainer}>
+            <>
+              <View style={TestInProgressStyle.questionPointContainer}>
+                <Paragraph theme="smallMedium" color="gray">
+                  Câu hỏi {indexQuestionCurrent + 1}
+                </Paragraph>
+                <Paragraph theme="smallMedium" color="gray">
+                  {questionList[indexQuestionCurrent].point} điểm
+                </Paragraph>
+              </View>
+
+              <VerticalSpacer size="l" />
+              <Paragraph theme="baseBold">
+                {questionList[indexQuestionCurrent].question}
               </Paragraph>
-              <Paragraph theme="smallMedium" color="gray">
-                {questionList[indexQuestionCurrent].point} điểm
-              </Paragraph>
-            </View>
 
-            <VerticalSpacer size="l" />
-            <Paragraph theme="baseBold">
-              {questionList[indexQuestionCurrent].question}
-            </Paragraph>
+              <VerticalSpacer size="xl" />
+              {questionList[indexQuestionCurrent].answers.map(
+                (answer, index) => {
+                  return (
+                    <View key={`answer-${index}`}>
+                      <AnswerButton
+                        answer={answer.answer}
+                        index={index}
+                        status={stateAnswerList[index]}
+                        onPress={() =>
+                          handleAnswerSelection(index, answer.isCorrect)
+                        }
+                      />
+                      <VerticalSpacer size="xl" />
+                    </View>
+                  );
+                }
+              )}
 
-            <VerticalSpacer size="xl" />
-            {questionList[indexQuestionCurrent].answers.map((answer, index) => {
-              return (
-                <View key={`answer-${index}`}>
-                  <AnswerButton
-                    answer={answer.answer}
-                    index={index}
-                    status={stateAnswerList[index]}
-                    onPress={() =>
-                      handleAnswerSelection(index, answer.isCorrect)
-                    }
-                  />
-                  <VerticalSpacer size="xl" />
-                </View>
-              );
-            })}
-
-            <VerticalSpacer size="xxl" />
-            <View style={{ paddingHorizontal: 100 }}>
-              <PrimaryBtn
-                title="Tiếp tục"
-                radius="full"
-                onPress={handleNextQuestion}
-                fontSize="medium"
-                disabled={isDisableContinute}
-              />
-            </View>
-          </>
-        </View>
+              <VerticalSpacer size="xxl" />
+              <View style={{ paddingHorizontal: 100 }}>
+                <PrimaryBtn
+                  title="Tiếp tục"
+                  radius="full"
+                  onPress={handleNextQuestion}
+                  fontSize="medium"
+                  disabled={isDisableContinute}
+                />
+              </View>
+            </>
+          </View>
+        </Animated.View>
       </View>
     </CustomerTemplate>
   );
