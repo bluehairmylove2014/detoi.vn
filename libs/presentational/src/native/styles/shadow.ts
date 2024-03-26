9c60f464e85b2497e994ae1dfc2d07b35d4ca405
TanKaka123 <hongtan1422002@gmail.com>
2024-03-26 07:41:41 +0700

    add more style shadow

--- libs/presentational/src/native/styles/shadow.ts
+++ libs/presentational/src/native/styles/shadow.ts
@@ -10,7 +10,46 @@ export const commonShadow = StyleSheet.create({
     },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
-
+    elevation: 5,
+  },
+  bottom: {
+    shadowColor: COLOR_PALETTE.black,
+    shadowOffset: {
+      width: 0,
+      height: -5, // Negative value to create a shadow below
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 5,
+  },
+  left: {
+    shadowColor: COLOR_PALETTE.black,
+    shadowOffset: {
+      width: -5, // Negative value to create a shadow on the left
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 5,
+  },
+  right: {
+    shadowColor: COLOR_PALETTE.black,
+    shadowOffset: {
+      width: 5, // Positive value to create a shadow on the right
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 5,
+  },
+  around: {
+    shadowColor: COLOR_PALETTE.black,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3.65,
     elevation: 5,
   },
 });
