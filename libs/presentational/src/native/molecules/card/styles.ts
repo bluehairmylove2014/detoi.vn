f1e4c97b01a31148b73be08ca8529d441af3d034
TanKaka123 <hongtan1422002@gmail.com>
2024-03-26 07:43:07 +0700

    change width freelancerCardOrderStyles

--- libs/presentational/src/native/molecules/card/styles.ts
+++ libs/presentational/src/native/molecules/card/styles.ts
@@ -23,6 +23,50 @@ export const ServiceCardSectionStyle = StyleSheet.create({
 export const freelancerCardOrderStyles = StyleSheet.create({
   container: {
     height: 200,
+    width:"92%",
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  imgBackground: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+  },
+  marginView: {
+    width: '100%',
+    height: '30%',
+  },
+  topTextView: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+  bottomTextView: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+    gap: 30,
+  },
+});
+
+
+export const freelancerCardAuctionStyles = StyleSheet.create({
+  container: {
+    height:200,
+    width:"92%",
     borderRadius: 10,
     overflow: 'hidden',
   },
