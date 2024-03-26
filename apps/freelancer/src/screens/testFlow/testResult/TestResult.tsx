42327b5d726b38d25e4049d287071118a05f623e
ptminngoc <pngoc0105@gmail.com>
2024-03-26 12:58:11 +0700

    ADD/View Container For SafeAreaView

--- apps/freelancer/src/screens/testFlow/testResult/TestResult.tsx
+++ apps/freelancer/src/screens/testFlow/testResult/TestResult.tsx
@@ -20,47 +20,49 @@ const TestResult: React.FC<
 
   return (
     <FreelancerTemplate>
-      <SafeAreaView style={TestResultStyle.safeAreaStyle}>
-        <View style={TestResultStyle.container}>
-          <View>
-            <View style={{ alignItems: 'center' }}>
-              {isSuccess ? (
-                <SquareImage source={require('./successTest.png')} />
-              ) : (
-                <SquareImage source={require('./failTest.png')} />
-              )}
+      <View style={TestResultStyle.safeAreaContainer}>
+        <SafeAreaView>
+          <View style={TestResultStyle.container}>
+            <View>
+              <View style={{ alignItems: 'center' }}>
+                {isSuccess ? (
+                  <SquareImage source={require('./successTest.png')} />
+                ) : (
+                  <SquareImage source={require('./failTest.png')} />
+                )}
+              </View>
+
+              <VerticalSpacer size="xxxl" />
+
+              <Title theme="largeBold" align="center" color="primary">
+                {isSuccess ? 'Chúc mừng!' : 'Xin chia buồn!'}
+              </Title>
+              <Paragraph theme="smallMedium" color="primary" align="center">
+                Bạn đã hoàn thành bài kiểm tra với{' '}
+                <Paragraph theme="smallBold" color="primary">
+                  {pointTest}
+                </Paragraph>{' '}
+                điểm
+              </Paragraph>
+              <Paragraph theme="smallMedium" color="primary" align="center">
+                ( Để làm lại, chọn Tài khoản {'>'} Làm kiểm tra kỹ năng )
+              </Paragraph>
+
+              <VerticalSpacer size="xxxl" />
+              <VerticalSpacer size="xxxl" />
+              <VerticalSpacer size="xxxl" />
+              <PrimaryBtn
+                radius="full"
+                title="Đến trang chủ"
+                onPress={() =>
+                  navigateToScreenInDifferentStack('HomeStack', 'Home')
+                }
+                isFitContent={true}
+              />
             </View>
-
-            <VerticalSpacer size="xxxl" />
-
-            <Title theme="largeBold" align="center" color="primary">
-              {isSuccess ? 'Chúc mừng!' : 'Xin chia buồn!'}
-            </Title>
-            <Paragraph theme="smallMedium" color="primary" align="center">
-              Bạn đã hoàn thành bài kiểm tra với{' '}
-              <Paragraph theme="smallBold" color="primary">
-                {pointTest}
-              </Paragraph>{' '}
-              điểm
-            </Paragraph>
-            <Paragraph theme="smallMedium" color="primary" align="center">
-              ( Để làm lại, chọn Tài khoản {'>'} Làm kiểm tra kỹ năng )
-            </Paragraph>
-
-            <VerticalSpacer size="xxxl" />
-            <VerticalSpacer size="xxxl" />
-            <VerticalSpacer size="xxxl" />
-            <PrimaryBtn
-              radius="full"
-              title="Đến trang chủ"
-              onPress={() =>
-                navigateToScreenInDifferentStack('HomeStack', 'Home')
-              }
-              isFitContent={true}
-            />
           </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </View>
     </FreelancerTemplate>
   );
 };
